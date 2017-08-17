1f71bbd66724ce7dad0f3d0cec472c694f6496c2
Elice-kim <grit_julia@naver.com>
2017-08-17 11:29:34 +0900

    add google library and json file

--- LoginApi/app/src/main/java/elice/me/loginapi/MainActivity.java
+++ LoginApi/app/src/main/java/elice/me/loginapi/MainActivity.java
@@ -1,13 +1,41 @@
 package elice.me.loginapi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int RC_SIGN_IN = 100;
+
+    @BindView(R.id.sign_in_button)
+    SignInButton googleSignBtn;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, null)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
     }
+    
 }
