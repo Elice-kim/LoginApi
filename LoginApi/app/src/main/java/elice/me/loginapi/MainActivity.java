15335289113fbc1401151fedeee06788729a7d4b
Elice-kim <grit_julia@naver.com>
2017-08-17 11:30:40 +0900

    click google login button and get result

--- LoginApi/app/src/main/java/elice/me/loginapi/MainActivity.java
+++ LoginApi/app/src/main/java/elice/me/loginapi/MainActivity.java
@@ -1,15 +1,20 @@
 package elice.me.loginapi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,5 +42,34 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
     }
-    
+    @OnClick(R.id.sign_in_button)
+    void googleLoginClicked() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RC_SIGN_IN && resultCode==RESULT_OK){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("sign", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            GoogleSignInAccount account = result.getSignInAccount();
+
+            Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
+            intent.putExtra("email", account.getEmail());
+            intent.putExtra("id", account.getId());
+            intent.putExtra("name", account.getDisplayName());
+            startActivity(intent);
+        } else {
+            // Signed out, show unauthenticated UI.
+        }
+
+    }
 }
