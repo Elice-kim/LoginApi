6558ec07b3544cdcf6fe821dd72f9fb1f14a544a
Elice-kim <grit_julia@naver.com>
2017-08-17 17:29:38 +0900

    facebook and google login api

--- LoginApi/app/src/main/java/elice/me/loginapi/MainActivity.java
+++ LoginApi/app/src/main/java/elice/me/loginapi/MainActivity.java
@@ -5,13 +5,29 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,10 +37,18 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int RC_SIGN_IN = 100;
 
-    @BindView(R.id.sign_in_button)
+    @BindView(R.id.googleLoginBtn)
     SignInButton googleSignBtn;
+    @BindView(R.id.facebookLoginBtn)
+    LoginButton facebookLoginBtn;
+    @BindView(R.id.googleLogoutBtn)
+    Button googleLogoutBtn;
 
     private GoogleApiClient mGoogleApiClient;
+    private CallbackManager callbackManager;
+    private String name;
+    private String email;
+    private String id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +57,49 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         mGoogleApiClient = makeGoogleApiClient(this);
+        callbackManager = CallbackManager.Factory.create();
+        facebookLoginClicked();
+        googleLogoutBtn.setVisibility(View.GONE);
+    }
 
+    private void facebookLoginClicked() {
+        facebookLoginBtn.setReadPermissions("public_profile", "email");
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                try {
+                                    Intent intent = new Intent(MainActivity.this,
+                                            UserInfoActivity.class);
+                                    intent.putExtra("name", object.getString("name"));
+                                    intent.putExtra("email", object.getString("email"));
+                                    intent.putExtra("id", object.getString("id"));
+                                    startActivity(intent);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,email,name");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "Facebook 로그인 취소", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private GoogleApiClient makeGoogleApiClient(FragmentActivity fragmentActivity) {
@@ -43,12 +109,12 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         return new GoogleApiClient.Builder(this)
-                .enableAutoManage(fragmentActivity , null)
+                .enableAutoManage(fragmentActivity, null)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
     }
 
-    @OnClick(R.id.sign_in_button)
+    @OnClick(R.id.googleLoginBtn)
     void googleLoginClicked() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
@@ -60,7 +126,9 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == RC_SIGN_IN && resultCode == RESULT_OK) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
+            return;
         }
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     private void handleSignInResult(GoogleSignInResult result) {
@@ -73,8 +141,26 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("id", account.getId());
             intent.putExtra("name", account.getDisplayName());
             startActivity(intent);
+
+            googleSignBtn.setVisibility(View.GONE);
+            googleLogoutBtn.setVisibility(View.VISIBLE);
+            googleLogoutBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                            new ResultCallback<Status>() {
+                                @Override
+                                public void onResult(Status status) {
+                                    Toast.makeText(MainActivity.this, "로그아웃 성공",
+                                            Toast.LENGTH_SHORT).show();
+                                    googleLogoutBtn.setVisibility(View.GONE);
+                                    googleSignBtn.setVisibility(View.VISIBLE);
+                                }
+                            });
+                }
+            });
         } else {
-            // Signed out, show unauthenticated UI.
+            Toast.makeText(this, "구글 로그인 실패", Toast.LENGTH_SHORT).show();
         }
 
     }
---
+++ LoginApi/app/src/main/java/elice/me/loginapi/wrapper/FacebookLoginWrapper.java
@@ -0,0 +1,11 @@
+package elice.me.loginapi.wrapper;
+
+/**
+ * Created by ladmusician.kim on 17/08/2017.
+ */
+
+public class FacebookLoginWrapper {
+
+
+
+}
